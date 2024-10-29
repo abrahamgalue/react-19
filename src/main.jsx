9c2a9d39e01d356543d63508083803483dec4b0b
Abraham Galue <abrahamgalue512@gmail.com>
2024-10-29 14:28:05 -0400

    React 19 new features work on SSR

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { renderToString } from 'react-dom/server'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
+
+// Next.js, Remix y otros...
+console.log(renderToString(<App />))
